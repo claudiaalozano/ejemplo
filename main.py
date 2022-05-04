25eedb7a23497e0113e538d0d57baf15d9ff0516
claudiaalozano <clau.lozano.perez@gmail.com>
2022-05-04 18:22:34 +0200

    commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+
 import numpy as np
 import matplotlib.pyplot as plt
 import csv
@@ -6,8 +7,14 @@ import os
 from csv import writer
 
 with open("ejemplo.csv", "a" , newline= " ") as f_object:
-    writer_obect = writer(f_object)
-    columna_random = np.random.rand(0,5, size=len(df1))
-    writer_obect.writerow(columna_random)
+    writer_object = writer(f_object)
+    columna_random = np.random.rand(0,5)
+    writer_object.writerow(columna_random)
     f_object.close()
 
+#segunda prueba para añadir columna
+
+ejemplo = pd.read_csv("ejemplo.csv", header= 0)
+df=pd.DataFrame(ejemplo)
+df.columna_random = np.random.rand(0,5)
+df.to_csv("ejemplo.csv", index=False)
