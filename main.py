e6162c4aad02e6a92ec275b28c63353d0caebf0a
claudiaalozano <clau.lozano.perez@gmail.com>
2022-05-04 19:31:54 +0200

    commit

--- main.py
+++ main.py
@@ -6,15 +6,19 @@ import csv
 import os
 from csv import writer
 
-with open("ejemplo.csv", "a" , newline= " ") as f_object:
-    writer_object = writer(f_object)
-    columna_random = np.random.rand(0,5)
-    writer_object.writerow(columna_random)
-    f_object.close()
+ejemplo = pd.DataFrame()
+df = pd.DataFrame(np.random.randint(0, 5, size=(100, 4)), columns=["aleatorio"])
+df.to_csv("aleatorio.csv")
 
 #segunda prueba para añadir columna
 
-ejemplo = pd.read_csv("ejemplo.csv", header= 0)
-df=pd.DataFrame(ejemplo)
-df.columna_random = np.random.rand(0,5)
-df.to_csv("ejemplo.csv", index=False)
+#ejemplo = pd.read_csv("ejemplo.csv", header= 0)
+#df=pd.DataFrame(ejemplo)
+#df.columna_random = np.random.rand(0,5)
+#df.to_csv("ejemplo.csv", index=False)
+
+#añadir columna a ejemplo.csv 
+#ejemplo = pd.read_csv("ejemplo.csv", header= 0)
+#df=pd.DataFrame(ejemplo)
+#df.columna_random = np.random.rand(0,5)
+#df.to_csv("ejemplo.csv", index=False)
