2a357af91432f45e23c142f67d956ee1dc2f310e
claudiaalozano <clau.lozano.perez@gmail.com>
2022-05-04 18:06:19 +0200

    columna random

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-import 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+import os
+from csv import writer
+
+with open("ejemplo.csv", "a" , newline= " ") as a f_object:
+    writer_obect = writer(f_object)
+    columna_random = np.random.rand(0,5, size=len(df1))
+    writer_obect.writerow(columna_random)
+    f_object.close()
+    
