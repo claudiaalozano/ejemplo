a2db1a3031e1f67a0ea8aae60fe8c98527a5a841
claudiaalozano <clau.lozano.perez@gmail.com>
2022-05-04 18:07:10 +0200

    commit

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ import csv
 import os
 from csv import writer
 
-with open("ejemplo.csv", "a" , newline= " ") as a f_object:
+with open("ejemplo.csv", "a" , newline= " ") as f_object:
     writer_obect = writer(f_object)
     columna_random = np.random.rand(0,5, size=len(df1))
     writer_obect.writerow(columna_random)
     f_object.close()
-    
+
